805f90f5faa4900102bc6b5f5941c989622ac428
caseylholston <caseylholston@gmail.com>
2017-04-04 13:19:18 -0400

    Deleted extraneous code

--- liri.js
+++ liri.js
@@ -3,54 +3,49 @@ var spotify = require('spotify');
 var request = require('request');
 var keys = require('./keys.js');
 var action = '';
-// var error = function (err, response, body) {
-//     	console.log('ERROR [%s]', err);
-// 	};
-// 	var success = function (data) {
-//     	console.log('Data [%s]', data);
-// 	};
 
  console.log(keys.twitterKeys);
  console.log(process.argv);
- return;
 
 
-if (process.argv[3] = 'my-tweets') {
 
-    var client = new twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret
+if (process.argv[3] == 'my-tweets') {
+    console.log('You typed my tweets');
+    // var client = new twitter({
+    // consumer_key: keys.twitterKeys.consumer_key,
+    // consumer_secret: keys.twitterKeys.consumer_secret,
+    // access_token_key: keys.twitterKeys.access_token_key,
+    // access_token_secret: keys.twitterKeys.access_token_secret
 
-    });
+    // });
 
-    var params = {screen_name: 'caseylholston', count: '20'};
+    // var params = {screen_name: 'caseylholston', count: '20'};
         
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    //     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         
-            if (!error) {
-            console.log(tweets);
-            }
-            else{console.log(error)
-            }
-        });
+    //         if (!error) {
+    //         console.log(tweets);
+    //         }
+    //         else{console.log(error)
+    //         }
+    //     });
     }
-else if (process.argv[3]= 'spotify-this-song') {
+else if (process.argv[3] == 'spotify-this-song') {
+    console.log('You typed spotify this song')
 
-        spotify.search({ type: 'track', query: process.argv[4] }, function(err, data) {
-            if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
-            }
-
-        });
+        // spotify.search({ type: 'track', query: process.argv[4] }, function(err, data) {
+        //     if ( err ) {
+        //     console.log('Error occurred: ' + err);
+        //     return;
+        //     }
+        //     console.log(data);
+        // });
     }
 
-else if (process.argv[3] = 'movie-this') {
+else if (process.argv[3] == 'movie-this') {
 
 
     
-}
+};
 
 
