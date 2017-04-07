27223f8c06c5e34eabc9e9e22c7589cac49fadaf
caseylholston <caseylholston@gmail.com>
2017-04-07 13:34:08 -0400

    The Sign as the defualt Spotify search is working

--- liri.js
+++ liri.js
@@ -38,15 +38,53 @@ if (process.argv[2] === 'my-tweets') {
                 }
         });
     }
-else if (process.argv[2] === 'spotify-this-song') {
-    console.log('You typed spotify this song');
+        else if (process.argv[2] === 'spotify-this-song') {
+            spotifyThis(process.argv[3]);
+        }
+        else if (process.argv[2] === 'movie-this') {
+            movieThis(process.argv[3]);
+        }
+        else if (process.argv[2] === 'do-what-it-says'){
+             doWhatItSays();
+        }
+//-------------------FUNCTIONS------------------------------------
 
-        spotify.search({ type: 'track', query: process.argv[3], limit: 20 }, function(err, data) {
+
+
+
+
+function spotifyThis(trackName) {
+    console.log('You typed spotify this song');
+        if (trackName === undefined) {
+            song = '0hrBpAOgrt8RXigk83LLNE';
+            spotify.lookup({ type: 'track', id: song, limit: 20 }, function(err, data) {
             if ( err ) {
             console.log('Error occurred: ' + err);
             return;
             }
-            console.log(JSON.stringify(data, null, 2));
+            //console.log(JSON.stringify(data, null, 2));
+            for (var i = 0; i < data.artists.length; i++) {
+                console.log('Artist(s): '+ JSON.stringify(data.artists[i].name, null, 2));
+            }
+
+            //console.log('Artist(s): '+ JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+            console.log('Song: '+ JSON.stringify(data.name, null, 2));
+            console.log('Preview Link: '+ JSON.stringify(data.external_urls.spotify, null, 2));
+            console.log('Album: '+ JSON.stringify(data.album.name, null, 2));
+        })
+        
+         }
+
+        else {
+            song = trackName; 
+
+        console.log(song);
+        spotify.search({ type: 'track', query: song, limit: 20 }, function(err, data) {
+            if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+            }
+            //console.log(JSON.stringify(data, null, 2));
             for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
                 console.log('Artist(s): '+ JSON.stringify(data.tracks.items[0].artists[i].name, null, 2));
             }
@@ -55,16 +93,9 @@ else if (process.argv[2] === 'spotify-this-song') {
             console.log('Song: '+ JSON.stringify(data.tracks.items[0].name, null, 2));
             console.log('Preview Link: '+ JSON.stringify(data.tracks.items[0].album.external_urls.spotify, null, 2));
             console.log('Album: '+ JSON.stringify(data.tracks.items[0].album.name, null, 2));
-        });
-    }
-
-        else if (process.argv[2] === 'movie-this') {
-            movieThis(process.argv[3]);
+            })
         }
-        else if (process.argv[2] === 'do-what-it-says'){
-             doWhatItSays();
-        }
-//-------------------FUNCTIONS------------------------------------
+}
 
 function movieThis(movie) {
     console.log(movie);
@@ -106,5 +137,3 @@ function movieThis(movie) {
 
     })
 };
-
- 
