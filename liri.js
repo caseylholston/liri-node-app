d14e195a95fd8ddbdf9e1ebd4b1ba269e45965a8
caseylholston <caseylholston@gmail.com>
2017-04-07 16:47:39 -0400

    Log and console has been prettified, added callback to get rid of appendFile error, not sure if that is done up to best practices

--- liri.js
+++ liri.js
@@ -30,12 +30,14 @@ var questions = [
  inquirer.prompt(questions).then(function (answers){
     var action = answers.action;
     var searchItem = answers.searchItem; 
-    fs.appendFile('log.txt', '---Action: ' + action + '---\n');
-    fs.appendFile('log.txt', '---Search Item: ' + searchItem + '---\n')
+    fs.appendFile('log.txt', '---Action: ' + action + '---\n', function(err){
+        if (err) (console.log('Error: ' + err))});
+    fs.appendFile('log.txt', '---Search Item: ' + searchItem + '---\n\n', function (err){
+        if (err) (console.log('Error: ' + err))});
 
     switchThis(action,searchItem);
-    console.log('Action: ' + action);
-    console.log('Search Item: ' + searchItem);
+    //console.log('Action: ' + action);
+    //console.log('Search Item: ' + searchItem +);
  })
  
 
@@ -63,7 +65,7 @@ function switchThis (answersAction,answersSearchItem) {
  }
 
 function tweetThis() {
-            // console.log('You typed my tweets');
+    // console.log('You typed my tweets');
     var client = new twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
@@ -80,7 +82,8 @@ function tweetThis() {
             //console.log(JSON.stringify(tweets).text, null, 2);
                 for( var i =0; i <20; i++) {
                    console.log(JSON.stringify(tweets[i].text, null, 2));
-                   fs.appendFile('log.txt', JSON.stringify(tweets[i].text, null, 2)+ '\n\n');
+                   fs.appendFile('log.txt', JSON.stringify(tweets[i].text, null, 2)+ '\n\n', function(err){
+                    if (err) (console.log('Error: ' + err))});
                 
                 }
             }
@@ -99,19 +102,25 @@ function spotifyThis(trackName) {
             console.log('Error occurred: ' + err);
             return;
             }
-            //console.log(JSON.stringify(data, null, 2));
+            console.log('\n\nYou Did Not Choose a Song it Must be Some Kind of Sign\n');
+            fs.appendFile('You Did Not Choose a Song it Must be Some Kind of Sign\n\n', function(err){
+                    if (err) (console.log('Error: ' + err))});
             for (var i = 0; i < data.artists.length; i++) {
                 console.log('Artist(s): '+ JSON.stringify(data.artists[i].name, null, 2));
-                fs.appendFile('log.txt', 'Artist(s): '+ JSON.stringify(data.artists[i].name, null, 2) + '\n');
+                fs.appendFile('log.txt', 'Artist(s): '+ JSON.stringify(data.artists[i].name, null, 2) + '\n', function(err){
+                    if (err) (console.log('Error: ' + err))});
             }
 
             //console.log('Artist(s): '+ JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
             console.log('Song: '+ JSON.stringify(data.name, null, 2));
-            fs.appendFile('log.txt', 'Song: '+ JSON.stringify(data.name, null, 2) + '\n');
-            console.log('Preview Link: '+ JSON.stringify(data.external_urls.spotify, null, 2));
-            fs.appendFile('log.txt', 'Preview Link: '+ JSON.stringify(data.external_urls.spotify, null, 2) + '\n');
+            fs.appendFile('log.txt', 'Song: '+ JSON.stringify(data.name, null, 2) + '\n', function(err){
+             if (err) (console.log('Error: ' + err))});
+            console.log('Preview Link: '+ JSON.stringify(data.external_urls.spotify, null, 2))
+            fs.appendFile('log.txt', 'Preview Link: '+ JSON.stringify(data.external_urls.spotify, null, 2) + '\n', function(err){
+             if (err) (console.log('Error: ' + err))});
             console.log('Album: '+ JSON.stringify(data.album.name, null, 2));
-            fs.appendFile('log.txt', 'Album: '+ JSON.stringify(data.album.name, null, 2) + '\n\n');
+            fs.appendFile('log.txt', 'Album: '+ JSON.stringify(data.album.name, null, 2) + '\n\n', function(err){
+             if (err) (console.log('Error: ' + err))});
         })
         
          }
@@ -119,43 +128,48 @@ function spotifyThis(trackName) {
         else {
             song = trackName; 
 
-        console.log(song);
+        //console.log(song);
         spotify.search({ type: 'track', query: song, limit: 5 }, function(err, data) {
             if ( err ) {
             console.log('Error occurred: ' + err);
             return;
         }
-            console.log('-----TOP 10 SEARCH RESULTS-----');
-            fs.appendFile('log.txt','-----TOP 10 SEARCH RESULTS-----');
+            console.log('-----TOP 10 SEARCH RESULTS-----\n');
+            fs.appendFile('log.txt','-----TOP 10 SEARCH RESULTS-----\n', function(err){
+             if (err) (console.log('Error: ' + err))});
             for (var i = 0; i < 9; i++) {
                 for (var i2 = 0; i2 < data.tracks.items[i].artists.length; i2++) {
                     console.log('Artist(s): '+ JSON.stringify(data.tracks.items[i].artists[i2].name, null, 2));
-                    fs.appendFile('log.txt', 'Artist(s): '+ JSON.stringify(data.tracks.items[i].artists[i2].name, null, 2) + '\n');
+                    fs.appendFile('log.txt', 'Artist(s): '+ JSON.stringify(data.tracks.items[i].artists[i2].name, null, 2) + '\n', function(err){
+                        if (err) (console.log('Error: ' + err))});
                 }
 
                 //console.log('Artist(s): '+ JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
                 console.log('Song: '+ JSON.stringify(data.tracks.items[i].name, null, 2));
-                fs.appendFile('log.txt', 'Song: '+ JSON.stringify(data.tracks.items[i].name, null, 2) + '\n');
+                fs.appendFile('log.txt', 'Song: '+ JSON.stringify(data.tracks.items[i].name, null, 2) + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
                 console.log('Preview Link: '+ JSON.stringify(data.tracks.items[i].album.external_urls.spotify, null, 2));
-                fs.appendFile('log.txt', 'Preview Link: '+ JSON.stringify(data.tracks.items[i].album.external_urls.spotify, null, 2) + '\n');
-                console.log('Album: '+ JSON.stringify(data.tracks.items[i].album.name, null, 2));
-                fs.appendFile('log.txt', 'Album: '+ JSON.stringify(data.tracks.items[i].album.name, null, 2) + '\n\n');            
+                fs.appendFile('log.txt', 'Preview Link: '+ JSON.stringify(data.tracks.items[i].album.external_urls.spotify, null, 2) + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+                console.log('Album: '+ JSON.stringify(data.tracks.items[i].album.name, null, 2) + '\n');
+                fs.appendFile('log.txt', 'Album: '+ JSON.stringify(data.tracks.items[i].album.name, null, 2) + '\n\n', function(err){
+                 if (err) (console.log('Error: ' + err))});           
                 }
             })
         }
 }
 
 function movieThis(movie) {
-    console.log(movie);
+    //console.log(movie);
     if (movie === '') {
           movieName = 'Mr. Nobody';
     }
     else {
         movieName = movie; 
     }
-    console.log(movieName);
+    //console.log(movieName);
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName.replace(/ /g, "+") + "&y=&plot=short&r=json";
-    console.log('Query URL: '+ queryUrl);
+    //console.log('Query URL: '+ queryUrl);
 
     request(queryUrl, function (error, response, body) {
         if( !error && response.statusCode ===200) {
@@ -170,15 +184,24 @@ function movieThis(movie) {
             console.log('The movie language is: ' + JSON.parse(body).Language);
             console.log('The Rotten Tomatoes score is: ' + JSON.parse(body).Ratings[1].Value);
 
-            fs.appendFile('log.txt', 'Title: ' + JSON.parse(body).Title + '\n');
-            fs.appendFile('log.txt', 'The movie was released in: ' + JSON.parse(body).Year + '\n');
-            fs.appendFile('log.txt', 'The IMDB rating is: ' + JSON.parse(body).Ratings[0].Value + '\n');
-            fs.appendFile('log.txt', 'The movie was produced in: ' + JSON.parse(body).Country) + '\n';
-            fs.appendFile('log.txt', 'The movie language is: ' + JSON.parse(body).Language + '\n');
-            fs.appendFile('log.txt', 'The movie plot is: ' + JSON.parse(body).Plot + '\n');
-            fs.appendFile('log.txt', 'Some actors in the movie are: ' + JSON.parse(body).Actors) + '\n';
-            fs.appendFile('log.txt', 'The movie language is: ' + JSON.parse(body).Language + '\n');
-            fs.appendFile('log.txt', 'The Rotten Tomatoes score is: ' + JSON.parse(body).Ratings[1].Value + '\n\n');
+            fs.appendFile('log.txt', 'Title: ' + JSON.parse(body).Title + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The movie was released in: ' + JSON.parse(body).Year + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The IMDB rating is: ' + JSON.parse(body).Ratings[0].Value + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The movie was produced in: ' + JSON.parse(body).Country + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The movie language is: ' + JSON.parse(body).Language + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The movie plot is: ' + JSON.parse(body).Plot + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'Some actors in the movie are: ' + JSON.parse(body).Actors + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The movie language is: ' + JSON.parse(body).Language + '\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
+            fs.appendFile('log.txt', 'The Rotten Tomatoes score is: ' + JSON.parse(body).Ratings[1].Value + '\n\n', function(err){
+                 if (err) (console.log('Error: ' + err))});
 
             }
         else {
